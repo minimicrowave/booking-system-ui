95e3bb900802f6141282ae05ff7eb45adefa440f
Serene <se-rene@outlook.com>
2021-12-09 02:34:18 +0800

    feat: add auth

--- src/common/hooks/useAPICall.ts
+++ src/common/hooks/useAPICall.ts
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import errorNotifcation from 'components/ErrorNotification';
 import errorMessages from 'common/constants/errorMessages';
+import { useNavigate } from 'react-router';
 
 function useAPICall(toShowErrorNotification: boolean = true) {
+    const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [response, setResponse] = useState<any>();
     const [hasError, setHasError] = useState(false);
@@ -19,6 +21,8 @@ function useAPICall(toShowErrorNotification: boolean = true) {
                 errorNotifcation(
                     errorMessages[error.response.status as number]
                 );
+
+            if (error.response.status === 401) navigate('/login');
         } finally {
             setIsLoading(false);
         }

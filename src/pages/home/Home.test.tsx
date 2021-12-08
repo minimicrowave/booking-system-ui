e859f8a4e73657f04c773f8997c221c4027c6c79
Serene <se-rene@outlook.com>
2021-12-09 05:34:34 +0800

    feat: add home page

---
+++ src/pages/home/Home.test.tsx
@@ -0,0 +1,28 @@
+import { Home } from 'pages';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { MemoryRouter } from 'react-router-dom';
+
+describe('<Home/>', () => {
+    const navBarId = 'bsui-navbar';
+    const cardId = 'bsui-card';
+    beforeEach(() => {
+        render(
+            <MemoryRouter>
+                <Home />
+            </MemoryRouter>
+        );
+    });
+
+    it('should render with navbar and cards', async () => {
+        const navBar = await screen.findByTestId(navBarId);
+        const cards = await screen.findAllByTestId(cardId);
+
+        expect(navBar).toBeTruthy();
+        expect(cards).toHaveLength(2);
+    });
+
+    // FIXME: RRD mocking required, to find out
+    it.skip('should redirect to their respective pages', () => {});
+});
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,44 @@
+import NavBar from 'components/NavBar';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Card } from 'antd';
+const { Meta } = Card;
+
+import './Home.less';
+
+const cards = [
+    {
+        title: 'Create Booking',
+        path: '/bookings/new',
+    },
+    {
+        title: 'My Bookings',
+        path: '/',
+    },
+];
+
+function Home() {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <NavBar />
+            <div className="card-container">
+                {cards.map(({ title, path }) => (
+                    <Card
+                        hoverable
+                        data-testid="bsui-card"
+                        className="card"
+                        key={title}
+                        onClick={() => navigate(path, { replace: true })}
+                    >
+                        <Meta title={title} />
+                    </Card>
+                ))}
+            </div>
+        </>
+    );
+}
+
+export default Home;
